548da62ef66dfb127a74775b734aeddba7011f8d
betwise1 <betwiseafrica1@gmail.com>
2026-01-16 23:40:08 +0000

    Fix mobile Tailwind rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,37 @@ export default {
     "./public/**/*.html",
     "./src/**/*.{js,ts}"
   ],
+
+  safelist: [
+    // Heights
+    "h-[500px]",
+    "min-h-[500px]",
+    "md:h-[550px]",
+
+    // Positioning
+    "absolute",
+    "relative",
+    "inset-0",
+    "overflow-hidden",
+    "z-10",
+
+    // Image utilities
+    "object-cover",
+    "brightness-75",
+
+    // Layout
+    "flex",
+    "items-center",
+    "justify-center",
+
+    // Text sizes
+    "text-2xl",
+    "text-3xl",
+    "md:text-3xl",
+    "text-4xl",
+    "md:text-5xl"
+  ],
+
   theme: {
     extend: {
       colors: {
@@ -11,5 +42,6 @@ export default {
       },
     },
   },
+
   plugins: [],
 };
