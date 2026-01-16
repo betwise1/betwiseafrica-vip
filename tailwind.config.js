6ef4558b9f062e26424ca60892ff84f1af4b885e
betwise1 <betwiseafrica1@gmail.com>
2026-01-16 18:15:34 +0000

    Fix Tailwind purge issue and mobile styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,27 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./public/**/*.html",
+    "./public/**/*.js",
+    "./src/**/*.js",
   ],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
       },
       colors: {
-        gold: '#d4af37',
-        yellowHover: '#facc15', // optional hover color
+        gold: "#d4af37",
+        yellowHover: "#facc15",
       },
     },
   },
+  safelist: [
+    "bg-gold",
+    "text-gold",
+    "border-gold",
+    "hover:bg-gold",
+    "hover:text-black",
+  ],
   plugins: [],
-}
+};
